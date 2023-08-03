925aced8444f04558878e76cc852b3e108cf5559
marcelo.filho <marceloandradesilvafilho@gmail.com>
2023-08-02 21:19:52 -0300

    - Removendo instanciação e utilizando lombok para classe e métodos

--- src/main/java/com/ead/authuser/controller/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controller/AuthenticationController.java
@@ -7,8 +7,7 @@ import com.ead.authuser.model.UserModel;
 import com.ead.authuser.service.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
@@ -25,7 +25,6 @@ import java.time.ZoneId;
 public class AuthenticationController {
 
     private final UserService userService;
-    Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody @JsonView(UserDTO.UserView.RegisterUser.class) @Validated(UserDTO.UserView.RegisterUser.class) UserDTO userDTO) {
@@ -50,11 +49,11 @@ public class AuthenticationController {
 
     @GetMapping("/")
     public String index() {
-        logger.trace("TRACE - GRANULARIDADE FINA, MUITOS DETALHES");
-        logger.debug("DEBUG - AMBIENTE DE DSV, IMPLEMENTAÇÃO DE CÓDIGO, VALORES DE VARIÁVEIS, ETC.");
-        logger.info("INFO - AMBIENTE DE PRD, INFORMAÇÕES DE PROCESSOS RELEVANTE PARA ARQUITETURA, SEM MUITA GRANULARIDADE");
-        logger.warn("WARN - PERDA DE DADOS SECUNDÁRIOS, PROCESSOS QUE OCORREM MAIS DE UMA VEZ (NÃO CHEGA A SER CONSIDERADO UM ERRO");
-        logger.error("ERROR - MUITO UTILIZADO EM BLOCOS TRY CATCH, ESTRUTURAR ERROS NO SISTEMA");
+        log.trace("TRACE - GRANULARIDADE FINA, MUITOS DETALHES");
+        log.debug("DEBUG - AMBIENTE DE DSV, IMPLEMENTAÇÃO DE CÓDIGO, VALORES DE VARIÁVEIS, ETC.");
+        log.info("INFO - AMBIENTE DE PRD, INFORMAÇÕES DE PROCESSOS RELEVANTE PARA ARQUITETURA, SEM MUITA GRANULARIDADE");
+        log.warn("WARN - PERDA DE DADOS SECUNDÁRIOS, PROCESSOS QUE OCORREM MAIS DE UMA VEZ (NÃO CHEGA A SER CONSIDERADO UM ERRO");
+        log.error("ERROR - MUITO UTILIZADO EM BLOCOS TRY CATCH, ESTRUTURAR ERROS NO SISTEMA");
         return "Logging Spring Boot...";
     }
 }
