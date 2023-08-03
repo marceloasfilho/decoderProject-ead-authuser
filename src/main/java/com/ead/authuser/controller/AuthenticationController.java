001d4f6d27f6a559733a9fd713e5e0008c058cb0
marcelo.filho <marceloandradesilvafilho@gmail.com>
2023-08-02 21:03:20 -0300

    - Adicionando logging com LogBack nativo do spring framework e seus níveis - Adicionando cores ao CMD quando iniciamos aplicação via command line - Criação de método teste para o logger

--- src/main/java/com/ead/authuser/controller/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.ead.authuser.model.UserModel;
 import com.ead.authuser.service.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,7 @@ import java.time.ZoneId;
 public class AuthenticationController {
 
     private final UserService userService;
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody @JsonView(UserDTO.UserView.RegisterUser.class) @Validated(UserDTO.UserView.RegisterUser.class) UserDTO userDTO) {
@@ -43,5 +46,15 @@ public class AuthenticationController {
         userModel.setLastUpdateDateTime(LocalDateTime.now(ZoneId.of("UTC")));
         this.userService.save(userModel);
         return new ResponseEntity<>(userModel, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        logger.trace("TRACE - GRANULARIDADE FINA, MUITOS DETALHES");
+        logger.debug("DEBUG - AMBIENTE DE DSV, IMPLEMENTAÇÃO DE CÓDIGO, VALORES DE VARIÁVEIS, ETC.");
+        logger.info("INFO - AMBIENTE DE PRD, INFORMAÇÕES DE PROCESSOS RELEVANTE PARA ARQUITETURA, SEM MUITA GRANULARIDADE");
+        logger.warn("WARN - PERDA DE DADOS SECUNDÁRIOS, PROCESSOS QUE OCORREM MAIS DE UMA VEZ (NÃO CHEGA A SER CONSIDERADO UM ERRO");
+        logger.error("ERROR - MUITO UTILIZADO EM BLOCOS TRY CATCH, ESTRUTURAR ERROS NO SISTEMA");
+        return "Logging Spring Boot...";
     }
 }
